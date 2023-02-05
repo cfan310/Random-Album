0ec8ee9c4dffb9a02e8d7b52a4d6ee0fec765da9
cfan310 <cfanning43@gmail.com>
2023-02-05 10:27:04 -0800

    mvp complete

--- app.js
+++ app.js
@@ -16,18 +16,42 @@ function changeBtnColor() {
     generateBtn.style.backgroundColor = btnColors[randNum];
 };
 
-generateBtn.addEventListener('click', changeBtnColor);
+//generateBtn.addEventListener('click', changeBtnColor);
 
 // function to assign numbers to albums 
 
 function genRandAlbum() {
     // assign godweensatan a value of 1
     // first get random numbers 1-5
-    let randNum = Math.floor(Math.random() * 5 + 1);
+    let randNum = Math.floor(Math.random() * 9 + 1);
     console.log(randNum);
     if (randNum == 1) {
+        randomAlbum.src = "album-covers/godweensatan.jpg";
+    }
+    else if (randNum == 2) {
         randomAlbum.src = "album-covers/thePod.jpg";
+    }
+    else if (randNum == 3) {
+        randomAlbum.src = "album-covers/pureGuava.jpg";
+    }
+    else if (randNum == 4) {
+        randomAlbum.src = "album-covers/chocolateandcheese.jpg";
+    }
+    else if (randNum == 5) {
+        randomAlbum.src = "album-covers/countryGreats.jpg";
+    }
+    else if (randNum == 6) {
+        randomAlbum.src = "album-covers/theMollusk.jpg";
+    }
+    else if (randNum == 7) {
+        randomAlbum.src = "album-covers/whitePepper.jpg";
+    }
+    else if (randNum == 8) {
+        randomAlbum.src = "album-covers/quebec.png";
+    }
+    else if (randNum == 9) {
+        randomAlbum.src = "album-covers/laCucaracha.jpg";
     }
 }
 
-//genRandAlbum();
+generateBtn.addEventListener('click', genRandAlbum);
